e87fb41f556ddf3e7f5a5f6e16b9ab2a0330d6a5
GuanSuns <guanlin25>
2016-05-05 11:30:26 +0800

    Initial commit

---
+++ src/main/java/guan/suns/StudentCurriculaManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package guan.suns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class StudentCurriculaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCurriculaManagementSystemApplication.class, args);
+	}
+}

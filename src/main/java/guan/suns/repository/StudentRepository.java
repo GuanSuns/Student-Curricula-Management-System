06e51620467631f333c9545ebb579268d241cde8
GuanSuns <guanlin25>
2016-05-15 22:00:14 +0800

    Update

--- src/main/java/guan/suns/repository/StudentRepository.java
+++ src/main/java/guan/suns/repository/StudentRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by lenovo on 2016/5/5.
  */
+
 public interface StudentRepository extends JpaRepository<StudentPDM, String> {
 }
